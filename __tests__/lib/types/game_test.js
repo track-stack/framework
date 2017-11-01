3dcb91ea6aa4410c8e8c542d311377b2a2ad59da
Mike Kavouras <kavourasm@gmail.com>
2017-11-01 12:00:59 -0500

    game test

--- __tests__/lib/types/game_test.js
+++ __tests__/lib/types/game_test.js
@@ -2,7 +2,34 @@
 
 import { Game } from '../../../lib/types'
 
-test('works', () => {
-  console.log(new Game(1, {viewer: {}, opponent: {}}, 1, []))
-  expect(1).toEqual(1)
+test("initializes an instance from json", () => {
+  const turn = {
+    user_id: 1, 
+    answer: "all the small things", 
+    created_at: new Date(),
+    distance: 0,
+    has_exact_artist_match: false,
+    has_exact_name_match: true,
+    match: {
+      name: "All the Small Things", 
+      artist: "blink-182"
+    },
+    user_photo: "https://image.png"
+  }
+
+  const json = {
+    id: 20, 
+    players: { 
+      viewer: {id: 1, name: "Mike", image: "https://image.png"},
+      opponent: {id: 2, name: "Meagan", image: "https://image.png"}
+    },
+    status: 1,
+    turns: [turn]
+  }
+
+  const game = new Game.from(json)
+
+  expect(game.id).toEqual(20)
+  expect(game.turns.length).toBe(1)
+  expect(game.players).toEqual(json.players)
 });
