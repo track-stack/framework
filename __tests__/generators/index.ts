6b4a32e57a3bd5d929a7cf92953e649ccef6e9b6
Mike Kavouras <kavourasm@gmail.com>
2017-11-18 23:58:34 -0600

    closer to factories with generators

---
+++ __tests__/generators/index.ts
@@ -0,0 +1,5 @@
+/*jshint esversion: 6 */
+
+export { default as generateGame } from './game-generator'
+export { default as generateStack } from './stack-generator'
+export { default as generateTurn } from './turn-generator'
